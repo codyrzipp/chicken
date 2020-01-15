6ea0744682ad8c0b3660496d0c886141ef78f687
Cody Zipp <Czipp2@mail.greenriver.edu>
2020-01-15 14:43:51 -0500

    added a views directory and changed index

--- index.php
+++ index.php
@@ -5,5 +5,21 @@
 //Turn on error reporting -- this is critical!
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+//require the autoload file
+require ("vendor/autoload.php");
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view -> render('views/all-about-chickens.html');
+});
+
+//run fat free
+$f3 -> run();
+
 ?>
 
